1f2da434b45d626408d7e74f0556bde5fd5f3359
julien <none@none>
2013-01-23 22:59:32 -0500

    Added license for Qt.
    --HG--
    rename : license/GPLv2 => licenses/GPLv2
    rename : license/GPLv3 => licenses/GPLv3
    rename : license/LGPLv3 => licenses/LGPLv3

--- licensedialog.cpp
+++ licensedialog.cpp
@@ -7,22 +7,28 @@ LicenseDialog::LicenseDialog(QWidget *parent) :
 {
 	ui->setupUi(this);
 
-	QFile gpl3File(":/text/LICENSE");
+	QFile gpl3File(":/text/licenses/GPLv3");
 	if (gpl3File.open(QIODevice::ReadOnly | QIODevice::Text)) {
 		ui->gpl3Text->setPlainText(gpl3File.readAll());
 		gpl3File.close();
 	}
 
-	QFile gpl2File(":/text/LICENSE-MCRYPT");
+	QFile gpl2File(":/text/licenses/GPLv2");
 	if (gpl2File.open(QIODevice::ReadOnly | QIODevice::Text)) {
 		ui->gpl2Text->setPlainText(gpl2File.readAll());
 		gpl2File.close();
 	}
 
-	QFile lgpl3File(":/text/oxygen-icons/LICENSE");
+	QFile lgpl3File(":/text/licenses/LGPLv3");
 	if (lgpl3File.open(QIODevice::ReadOnly | QIODevice::Text)) {
 		ui->lgpl3Text->setPlainText(lgpl3File.readAll());
 		lgpl3File.close();
+	}
+
+	QFile lgpl21File(":/text/licenses/LGPLv21");
+	if (lgpl21File.open(QIODevice::ReadOnly | QIODevice::Text)) {
+		ui->lgpl21Text->setPlainText(lgpl21File.readAll());
+		lgpl21File.close();
 	}
 }
 
